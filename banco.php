03eb6e073326395aabbf6cc2da68f4fba501c0e7
Calebe-Finger <142498996+Calebe-Finger@users.noreply.github.com>
2024-12-02 21:27:57 -0300

    Add files via upload

---
+++ banco.php
@@ -0,0 +1,12 @@
+<?php
+
+    $id = $_POST['id'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['mensagem'];
+
+    $host = "localhost";
+    $database = "Doces Encanto";
+    $username = "root";
+    $password = "";
+?>
